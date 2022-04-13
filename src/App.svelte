a39906ef51cb135354e88a952e5a1cb50ab03403
Marina de la Fuente Gómez-Tavira <mdelafuente18@gmail.com>
2022-04-13 11:39:39 +0800

    Add quiz project and typescript
    - Add svelte project with App file and Quiz file
    - Add typescript to project

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Quiz from "./Quiz.svelte";
+</script>
+
+<main>
+  <Quiz />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+  /* Global styles */
+  :global(h2) {
+    color: aqua;
+  }
+
+  :global(button, a) {
+    cursor: pointer;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
